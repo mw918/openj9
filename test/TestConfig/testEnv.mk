7fad02058b2802c5212c5c9bb5fb29a5efb8b467
Michael Wang <Ziqing.Wang@ibm.com>
2019-09-17 10:18:39 -0400

    Removed export keyword

--- test/TestConfig/testEnv.mk
+++ test/TestConfig/testEnv.mk
@@ -24,7 +24,7 @@ testEnvSetup:
 
 testEnvTeardown:
 
-export TR_Options=verbose, vlog=testExample1.log;
+TR_Options=verbose, vlog=testExample1.log;
 
 ifneq (,$(findstring JITAAS,$(TEST_FLAG)))
 testEnvSetup:
